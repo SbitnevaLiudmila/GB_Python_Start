1fbf4a6b19d78215fc51c656b6a54959bf1fb151
Liudmila Sbitneva <gamma1974@yandex.ru>
2020-02-20 18:49:43 +0300

    Lesson5_answer

--- Lesson5/Task05_07.py
+++ Lesson5/Task05_07.py
@@ -11,7 +11,36 @@
 Пример json-объекта:
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 
-Подсказка: использовать менеджеры контекста.
+Подсказка: использовать менеджеры контекста."""
 
-r'\b\d+\b'
-Если вам нужны только числа, ограниченные границами слов (пробел, период, запятая), вы можете использовать \b:"""
+with open('firms.txt', 'r') as f:
+    firm_list = []
+    result_list = []
+    sum_fin_result = 0
+    count = 0
+    for line in f:
+        if len(line) < 4: # проверка на пустые строки в конце
+            continue
+        my_list = line.split()
+        if my_list[-1].isdigit() == False: continue    #чтобы не обрабатывалась первая строка с названиями столбцов
+        firm = my_list[0]
+        firm_list.append(firm)
+        fin_result = int(my_list[-2]) - int(my_list[-1])
+        result_list.append(fin_result)
+        if fin_result >= 0:
+            print(f'Прибыль {firm} равна {fin_result}')
+            sum_fin_result += fin_result
+            count += 1
+        else:
+            print(f'{firm} отработала с убытком {fin_result}')
+
+    average_profit = sum_fin_result / count
+    firm_dict = dict(zip(firm_list, result_list))
+    average_dict = {'average_profit': average_profit}
+    total_list = [firm_dict, average_dict]
+    print(total_list)
+
+import json
+with open('data.json', 'w') as f:
+    json.dump(total_list, f)
+
